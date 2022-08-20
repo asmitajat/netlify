605c7a9cbb0d4cc431307922e051e496f622ea04
asmitajat <73101427+asmitajat@users.noreply.github.com>
2022-08-20 18:26:19 +0200

    Update storehash.js

--- src/storehash.js
+++ src/storehash.js
@@ -1,7 +1,8 @@
 import web3 from './web3';
 //access our local copy to contract deployed on rinkeby testnet
 //use your own contract address
-const address = '0xE9fad4e22Ea82aC0db71117c2e773b0720db052d';
+//const address = '0xE9fad4e22Ea82aC0db71117c2e773b0720db052d';
+const address = '0x9ED35fDd7e3E6a57A1485d8e2201F12af33AfeD5';
 //use the ABI from your contract
 const abi = [
   
